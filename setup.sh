eb1d182f1e89bb7c8294edbfbb010bb1a8a7cdcb
Rajesh Jaga <23053792+rajeshjaga@users.noreply.github.com>
2025-08-12 10:23:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 sudo rm -rf /opt/basebuild
 sudo git clone https://github.com/nikithgowda22/LABSP /opt/basebuild
 sudo rm -rf /home/rit-cse/.bash_history
-nohup sh -c 'sleep 3 && rm -f /home/rit-cse/.bash_history' & disown && exit
+nohup sh -c 'sleep 3 && rm -f /home/rit-cse/.bash_history' >/dev/null 2>&1 & disown && exit
